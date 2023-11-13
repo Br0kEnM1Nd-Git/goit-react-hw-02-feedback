4db849740a806ebe95783188b89c108c7fbbe308
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-13 18:22:22 +0200

    upd

--- src/components/Feedback/FeedbackOptions.jsx
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -4,27 +4,17 @@ import { Button } from 'components/Helpers/Components.styled';
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <FeedbackButtonsChamber>
-      <Button
-        onClick={() => {
-          onLeaveFeedback(options[0]);
-        }}
-      >
-        Good
-      </Button>
-      <Button
-        onClick={() => {
-          onLeaveFeedback(options[1]);
-        }}
-      >
-        Neutral
-      </Button>
-      <Button
-        onClick={() => {
-          onLeaveFeedback(options[2]);
-        }}
-      >
-        Bad
-      </Button>
+      {options.map(option => {
+        return (
+          <Button
+            onClick={() => {
+              onLeaveFeedback(option);
+            }}
+          >
+            {option}
+          </Button>
+        );
+      })}
     </FeedbackButtonsChamber>
   );
 };
